721debb258da50d94a0da311efcf3adf2c68308f
Jesp <jstriker9peru@gmail.com>
2020-06-20 22:33:51 -0400

    setup react navigator

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
+import 'react-native-gesture-handler';
 import React from 'react';
+import HomeScreen from './screens/HomeScreen';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Colors from './constants/Colors';
+import RecipeScreen from './screens/RecipeScreen';
+import CategoryScreen from './screens/CategoryScreen';
+
+const Stack = createStackNavigator();
+
+const defaultStackNavOptions = {
+  headerStyle: {
+      backgroundColor: Colors.primaryColor,
+  },
+  headerTitleStyle: {
+      color: Colors.white
+  }
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={defaultStackNavOptions}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Category" component={CategoryScreen} />
+        <Stack.Screen name="Recipe" component={RecipeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
