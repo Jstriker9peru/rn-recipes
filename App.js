5f242ad8d3f69d161b9a1410a2bb85d21b830549
Jesp <jstriker9peru@gmail.com>
2020-06-24 20:50:11 -0400

    added navigation props to screens

--- App.js
+++ App.js
@@ -1,41 +1,45 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import HomeScreen from './screens/HomeScreen';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Colors from './constants/Colors';
-import RecipeScreen from './screens/RecipeScreen';
-import CategoryScreen from './screens/CategoryScreen';
+import "react-native-gesture-handler";
+import React from "react";
+import HomeScreen from "./screens/HomeScreen";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Colors from "./constants/Colors";
+import RecipeScreen from "./screens/RecipeScreen";
+import CategoryScreen from "./screens/CategoryScreen";
 
 const Stack = createStackNavigator();
 
 const defaultStackNavOptions = {
-  headerStyle: {
-      backgroundColor: Colors.primaryColor,
-  },
-  headerTitleStyle: {
-      color: Colors.white
-  }
+    headerStyle: {
+        backgroundColor: Colors.primaryColor,
+    },
+    headerTitleStyle: {
+        color: Colors.white,
+    },
 };
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={defaultStackNavOptions}>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Category" component={CategoryScreen} />
-        <Stack.Screen name="Recipe" component={RecipeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={defaultStackNavOptions}>
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen
+                    name="Category"
+                    component={CategoryScreen}
+                    options={({ route }) => ({ title: route.params.title })}
+                />
+                <Stack.Screen name="Recipe" component={RecipeScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
